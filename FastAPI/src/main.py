ca8ab9ef66e0c4b8e70dcb6aac9083c8ba146993
Tibbers Hao <tibbers@Tibberss-iMac.local>
2022-05-23 18:46:44 -0700

    Rewrite API Calls

--- FastAPI/src/main.py
+++ FastAPI/src/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, exceptions
 from elasticsearch_dsl import Search, Index, Document
 from ssl import create_default_context
+import json
 
 #----------Elastic Authentication----------#
 cert = create_default_context(cafile = '/app/fastapi/src/certs/ca/ca.crt')
@@ -48,137 +49,83 @@ def search(keyword: str) -> list:
         keyword: the keyword used to put into a search query
     Return:
         list of documents matching the search query, with order associated with ranking score.
-
     '''
     resp = Search().using(es).query("multi_match", query = keyword, fuzziness = "AUTO").extra(track_total_hits = True).execute()
     return list(resp)
 
 #----------POST----------#
-@app.post(API_URL_PREFIX + '/index/', tags = ['Index'])
+@app.post(API_URL_PREFIX + '/index/', status_code=201, tags = ['Index'])
 def create_index(req: NewIndex):
     '''
     Create a new index for elasticsearch.
 
     Args:
-        req: the name of index waiting for creation.
+        req: the name of index waiting for creation. (from request body)
 
     Return:
-
+        if index being created successfully -> response body 
+        if index already exists             -> 400 error
     '''
     try:
         resp = Index(req.index).create(using = es)
-        return [f'Index: \"{req.index}\" has been successfully created!', resp]
-
-    except Exception as e:
-        return f'Index: \"{req.index}\" already exists, please check.'
-
-@app.post(API_URL_PREFIX + '/index/document', tags = ['Document'])
-def index_doc(index: str, doc_id: str, doc: dict):
+    except exceptions.RequestError as e:
+        raise HTTPException(status_code = 400, detail = str(e))
+    else:
+        return resp
+    
+@app.post(API_URL_PREFIX + '/index/document', status_code=201, tags = ['Document'])
+def index_doc(index: str, doc_id: str, doc: NewDocument):
     '''
     Insert a document to the index.
 
     Args:
+        index: category of the document
+        doc_id: id of the document
+        doc: document content to be ingested
 
     Return:
-
+        response body, creat/update will be indicated
     '''
-    
-    check_index = Index(index).exists(using = es)
-    if not check_index:
-        return f'Index: \"{index}\" does not exist, please check.'
-    else:
-        check_doc = Document().exists(using = es, index = index, id = doc['uid'])
-        if check_doc:
-            es.index(index = index, id = doc['uid'], document = doc)
-            return 'UID: \"' + doc['uid'] + '\" has been updated.'
-        else:
-            es.index(index = index, id = doc['uid'], document = doc)
-            return f'New document has been successfully inserted in \"{index}\".'
+    resp = es.index(index = index, id = doc_id, document = dict(doc))
+    return resp
 
 #----------PUT----------#
 
 
 #----------DELETE----------#
-@app.delete(API_URL_PREFIX + '/index/{index}', tags = ['Index'])
+@app.delete(API_URL_PREFIX + '/index/{index}', status_code=204, tags = ['Index'])
 def delete_index(index: str):
     '''
     Delete an index.
 
     Args:
+        index: the name of index to be deleted
 
     Return:
-
+        if index being deleted successfully -> response body 
+        if index does not exist             -> 404 error
     '''
     try:
         resp = Index(index).delete(using = es)
-        return [f'Index: \"{index}\" has been successfully deleted!', resp]
-    except Exception as e:
-        return f'Index: \"{index}\" does not exist.'
-    
+    except exceptions.NotFoundError as e:
+        raise HTTPException(status_code = 404, detail = str(e))
+    else:
+        return resp
 
-@app.delete(API_URL_PREFIX + '/index/{index}/document/{doc_id}', tags = ['Document'])
+@app.delete(API_URL_PREFIX + '/index/{index}/document/{doc_id}', status_code = 204, tags = ['Document'])
 def delete_doc(index: str, doc_id: str):
     '''
     Delete the document within the index.
 
     Args:
+        index: the index where document resides
+        doc_id: the id of document to be deleted
 
     Return:
-
+        if document being deleted successfully -> HTTP 204 Status Code
+        if document id or index does not exist -> 404 error
     '''
-    
-    
-
-
-# class Item(BaseModel):
-#     name: str
-#     affiliation: str
-#     role: str
-#     uid: str
-#     #brand: Optional[str] = None
-
-# class UpdateItem(BaseModel):
-#     name: str
-#     affiliation: Optional[str] = None
-#     role: str
-#     uid: Optional[str] = None
-
-# @app.get('/get-by-name/{item_id}')
-# def get_item(*, item_id: int, name: Optional[str] = None, test: int):
-#     for item_id in inventory:
-#         if inventory[item_id].name == name:
-#             return inventory[item_id]
-#         raise HTTPException(status_code=404, detail = 'Item name not found.')
-
-# @app.post('/create-item/{item_id}')
-# def create_item(item_id: int, item: Item):
-#     if item_id in inventory:
-#         return {'Error': 'Item ID already exists.'}
-
-#     inventory[item_id] = item
-#     #doc[id] = doc
-
-
-#     return inventory[item_id]
-
-# @app.put('/update-item/{item_id}')
-# def update_item(item_id: int, item: UpdateItem):
-#     if item_id not in inventory:
-#         raise HTTPException(status_code=404, detail = 'Item name not found.')
-
-#     if item.name != None:
-#         inventory[item_id].name = item.name
-#     if item.price != None:
-#         inventory[item_id].price = item.price
-#     if item.brand != None:
-#         inventory[item_id].brand = item.brand
-
-#     return inventory[item_id]
-
-# @app.delete('/delete-item')
-# def delete_item(item_id: int = Query(..., description = 'The ID of the item to delete')):
-#     if item_id not in inventory:
-#         return {'Error': 'ID does not exist'}
-    
-#     del inventory[item_id]
-#     return {'Success': 'Item deleted!'}
+    try:
+        resp = Document().delete(using = es, index = index, id = doc_id)
+    except exceptions.NotFoundError as e:
+        raise HTTPException(status_code = 404, detail = str(e))

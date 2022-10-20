4024ae5d7d3d6b825bc4f77fe2fab08bd8d75fff
Tibbers Hao <ghao@lbl.gov>
2022-10-19 17:29:27 -0700

    update file_manager display

--- Dash-Fronty/src/app.py
+++ Dash-Fronty/src/app.py
@@ -22,6 +22,9 @@ from file_manager import filename_list, move_a_file, move_dir, docker_to_local_p
 import helper_utils
 import uuid
 
+# import from file_manager
+import base64, math
+
 
 #------------App Setup------------#
 external_stylesheets = [dbc.themes.BOOTSTRAP, "assets/segmentation-style.css"]
@@ -30,6 +33,9 @@ app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callbac
 
 #------------Global Variable-------------#
 USER = 'Dummy-Searcher' 
+
+NUMBER_OF_ROWS = 4
+NUMBER_IMAGES_PER_ROW = 4
 
 DOCKER_DATA = pathlib.Path.home().parent / 'data'
 LOCAL_DATA = str(os.environ['DATA_DIR'])
@@ -332,7 +338,7 @@ file_explorer = html.Div(
 # DISPLAY DATASET
 display = html.Div(
     [
-        file_explorer,
+        #file_explorer,
         html.Div(id='output-image-upload'),
         dbc.Row([
             dbc.Col(dbc.Row(dbc.Button('<', id='prev-page', style={'width': '10%'}, disabled=True), justify='end')),
@@ -460,8 +466,9 @@ image_search_card = dbc.Card(
         dbc.Label("Upload Image Here: "),   
         file_explorer,
         dbc.Row([
-            dbc.Col(dcc.Graph(id='raw-image-results', figure = blank_fig())),
-            dbc.Col(dcc.Graph(id='image-search-results', figure = blank_fig())),
+            # dbc.Col(dcc.Graph(id='raw-image-results', figure = blank_fig())),
+            dbc.Col(display, width = 6),
+            dbc.Col(dcc.Graph(id='image-search-results', figure = blank_fig()), width = 6),
         ])
 ])
 
@@ -626,27 +633,27 @@ def image_search(n_clicks, category, cnn, searching_method, number_of_images, co
     counts += 1
     return counts
 
-@app.callback(
-    Output('job-table', 'data'),
-    Input('job-refresher', 'n_intervals'),
-)
-def status_check(n):
-    url = f'http://job-service:8080/api/v0/jobs?&user={USER}&mlex_app=mlex_search'
-    # check the status of the job and show in the list
-    list_of_jobs = requests.get(url).json()
-    print(list_of_jobs, '------------')
-    data_table = []
-    for job in list_of_jobs:
-        data_table.insert(0, dict(job_id = job['job_kwargs']['kwargs']['job_id'],
-                                  job_type = job['job_kwargs']['kwargs']['job_type'],
-                                  status = job['status']['state'],
-                                  database = job['job_kwargs']['kwargs']['database'],
-                                  cnn = job['job_kwargs']['kwargs']['cnn'],
-                                  searching_method = job['job_kwargs']['kwargs']['searching_method'],
-                                  number_of_images = job['job_kwargs']['kwargs']['number_of_images'],
-                                  job_logs = job['logs'])
-        )
-    return data_table
+# @app.callback(
+#     Output('job-table', 'data'),
+#     Input('job-refresher', 'n_intervals'),
+# )
+# def status_check(n):
+#     url = f'http://job-service:8080/api/v0/jobs?&user={USER}&mlex_app=mlex_search'
+#     # check the status of the job and show in the list
+#     list_of_jobs = requests.get(url).json()
+#     print(list_of_jobs, '------------')
+#     data_table = []
+#     for job in list_of_jobs:
+#         data_table.insert(0, dict(job_id = job['job_kwargs']['kwargs']['job_id'],
+#                                   job_type = job['job_kwargs']['kwargs']['job_type'],
+#                                   status = job['status']['state'],
+#                                   database = job['job_kwargs']['kwargs']['database'],
+#                                   cnn = job['job_kwargs']['kwargs']['cnn'],
+#                                   searching_method = job['job_kwargs']['kwargs']['searching_method'],
+#                                   number_of_images = job['job_kwargs']['kwargs']['number_of_images'],
+#                                   job_logs = job['logs'])
+#         )
+#     return data_table
 
 @app.callback(
     Output('job-logs', 'value'),
@@ -829,28 +836,179 @@ def load_dataset(clear_data, browse_format, import_n_clicks, delete_n_clicks,
     else:
         return docker_to_local_path(files, DOCKER_HOME, LOCAL_HOME), selected_files
 
-@app.callback(
-    Output('raw-image-results', 'figure'),
-    Input('files-table', 'selected_rows'),
-    Input('docker-file-paths', 'data'),
-)
-def display_raw_image(row, selected_files):
-    if not row:
-        raise PreventUpdate
+# @app.callback(
+#     Output('raw-image-results', 'figure'),
+#     Input('files-table', 'selected_rows'),
+#     Input('docker-file-paths', 'data'),
+# )
+# def display_raw_image(row, selected_files):
+#     if not row:
+#         raise PreventUpdate
     
-    else:
-        if bool(selected_files):
+#     else:
+#         if bool(selected_files):
         
-            if selected_files[0]['file_type'] == 'file':
-                img_path = selected_files[0]['file_path']
-                img = np.array(imageio.imread(img_path))
-                img = parse_images(img)
-                return img
-            else:
-                raise PreventUpdate
-        else:
-            raise PreventUpdate
+#             if selected_files[0]['file_type'] == 'file':
+#                 img_path = selected_files[0]['file_path']
+#                 img = np.array(imageio.imread(img_path))
+#                 img = parse_images(img)
+#                 return img
+#             else:
+#                 raise PreventUpdate
+#         else:
+#             raise PreventUpdate
 # Maybe do clear out rather than prevent
+
+@app.callback(
+    Output('image-order','data'),
+    Input('docker-file-paths','data'),
+    Input('import-dir', 'n_clicks'),
+    Input('import-format', 'value'),
+    Input('files-table', 'selected_rows'),
+    Input('confirm-delete','n_clicks'),
+    Input('move-dir', 'n_clicks'),
+    State('image-order','data'),
+    prevent_initial_call=True)
+def display_index(file_paths, import_n_clicks, import_format, rows,
+                  delete_n_clicks, move_dir_n_clicks, image_order):
+    '''
+    This callback arranges the image order according to the following actions:
+        - New content is uploaded
+        - Buttons sort or hidden are selected
+    Args:
+        file_paths :            Absolute file paths selected from path table
+        import_n_clicks:        Button for importing selected paths
+        import_format:          File format for import
+        rows:                   Rows of the selected file paths from path table
+        delete_n_clicks:        Button for deleting selected file paths
+        image_order:            Order of the images according to the selected action (sort, hide, new data, etc)
+
+    Returns:
+        image_order:            Order of the images according to the selected action (sort, hide, new data, etc)
+        data_access_open:       Closes the reactive component to select the data access (upload vs. directory)
+    '''
+    supported_formats = []
+    import_format = import_format.split(',')
+    if import_format[0] == '*':
+        supported_formats = ['tiff', 'tif', 'jpg', 'jpeg', 'png']
+    else:
+        for ext in import_format:
+            supported_formats.append(ext.split('.')[1])
+
+    changed_id = dash.callback_context.triggered[0]['prop_id']
+    if import_n_clicks and bool(rows):
+        list_filename = []
+        if bool(file_paths):
+            for file_path in file_paths:
+                if file_path['file_type'] == 'dir':
+                    list_filename = add_paths_from_dir(file_path['file_path'], supported_formats, list_filename)
+                else:
+                    list_filename.append(file_path['file_path'])
+        else:
+            image_order = []
+    
+        num_imgs = len(list_filename)
+        if  changed_id == 'import-dir.n_clicks' or \
+            changed_id == 'confirm-delete.n_clicks' or \
+            changed_id == 'files-table.selected_rows' or \
+            changed_id == 'move_dir_n_clicks':
+            image_order = list(range(num_imgs))
+            
+    else:
+        image_order = []
+
+    
+    print(f'file paths 0 {file_paths}')
+    print(f'image order 0 {image_order}')
+    return image_order
+
+
+@app.callback([
+    Output('output-image-upload', 'children'),
+    Output('prev-page', 'disabled'),
+    Output('next-page', 'disabled'),
+    Output('current-page', 'data'),
+
+    Input('image-order', 'data'),
+    Input('prev-page', 'n_clicks'),
+    Input('next-page', 'n_clicks'),
+    Input('files-table', 'selected_rows'),
+    Input('import-format', 'value'),
+    Input('docker-file-paths','data'),
+    Input('my-toggle-switch', 'value'),
+
+    State('current-page', 'data'),
+    State('import-dir', 'n_clicks')],
+    prevent_initial_call=True)
+def update_output(image_order, button_prev_page, button_next_page, rows, import_format,
+                  file_paths, docker_path, current_page, import_n_clicks):
+    '''
+    This callback displays images in the front-end
+    Args:
+        image_order:            Order of the images according to the selected action (sort, hide, new data, etc)
+        button_prev_page:       Go to previous page
+        button_next_page:       Go to next page
+        rows:                   Rows of the selected file paths from path table
+        import_format:          File format for import
+        file_paths:             Absolute file paths selected from path table
+        docker_path:            Showing file path in Docker environment
+        current_page:           Index of the current page
+        import_n_clicks:        Button for importing the selected paths
+    Returns:
+        children:               Images to be displayed in front-end according to the current page index and # of columns
+        prev_page:              Enable/Disable previous page button if current_page==0
+        next_page:              Enable/Disable next page button if current_page==max_page
+        current_page:           Update current page index if previous or next page buttons were selected
+    '''
+    supported_formats = []
+    import_format = import_format.split(',')
+    if import_format[0] == '*':
+        supported_formats = ['tiff', 'tif', 'jpg', 'jpeg', 'png']
+    else:
+        for ext in import_format:
+            supported_formats.append(ext.split('.')[1])
+    
+    changed_id = dash.callback_context.triggered[0]['prop_id']
+    # update current page if necessary
+    if changed_id == 'image-order.data':
+        current_page = 0
+    if changed_id == 'prev-page.n_clicks':
+        current_page = current_page - 1
+    if changed_id == 'next-page.n_clicks':
+        current_page = current_page + 1
+
+    children = []
+    num_imgs = 0
+    if import_n_clicks and bool(rows):
+        list_filename = []
+        for file_path in file_paths:
+            if file_path['file_type'] == 'dir':
+                list_filename = add_paths_from_dir(file_path['file_path'], supported_formats, list_filename)
+            else:
+                list_filename.append(file_path['file_path'])
+    
+        # plot images according to current page index and number of columns
+        num_imgs = len(image_order)
+        if num_imgs>0:
+            start_indx = NUMBER_OF_ROWS * NUMBER_IMAGES_PER_ROW * current_page
+            max_indx = min(start_indx + NUMBER_OF_ROWS * NUMBER_IMAGES_PER_ROW, num_imgs)
+            new_contents = []
+            new_filenames = []
+            for i in range(start_indx, max_indx):
+                filename = list_filename[image_order[i]]
+                with open(filename, "rb") as file:
+                    img = base64.b64encode(file.read())
+                    file_ext = filename[filename.find('.')+1:]
+                    new_contents.append('data:image/'+file_ext+';base64,'+img.decode("utf-8"))
+                if docker_path:
+                    new_filenames.append(list_filename[image_order[i]])
+                else:
+                    new_filenames.append(docker_to_local_path(list_filename[image_order[i]], DOCKER_HOME, LOCAL_HOME, 'str'))
+                
+            children = helper_utils.draw_rows(new_contents, new_filenames, NUMBER_IMAGES_PER_ROW, NUMBER_OF_ROWS)
+
+    return children, current_page==0, math.ceil((num_imgs//NUMBER_IMAGES_PER_ROW)/NUMBER_OF_ROWS)<=current_page+1, \
+           current_page
 
 
 if __name__ == '__main__':
